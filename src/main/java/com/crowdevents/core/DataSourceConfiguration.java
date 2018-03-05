e6a38c092b605574112369eb50ab57f022ada2ea
Seregy <seregy77@gmail.com>
2018-03-05 18:57:33 +0200

    Use data from properties for initializing PostgreSQL database

--- src/main/java/com/crowdevents/core/DataSourceConfiguration.java
+++ src/main/java/com/crowdevents/core/DataSourceConfiguration.java
@@ -1,6 +1,7 @@
 package com.crowdevents.core;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,12 +22,17 @@ public class DataSourceConfiguration {
 
     @Profile("qa")
     @Bean
-    public DataSource qaDataSource() {
+    public DataSource qaDataSource(@Value("${POSTGRES_HOSTNAME}") String host,
+                                   @Value("${POSTGRES_PORT}") String port,
+                                   @Value("${POSTGRES_DB}") String database,
+                                   @Value("${POSTGRES_USER}") String user,
+                                   @Value("${POSTGRES_PASSWORD}") String password) {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/crowdevents-qa");
-        dataSource.setUsername("qa");
-        dataSource.setPassword("qapass");
+        dataSource.setUrl(String.format("jdbc:postgresql://%s:%s/%s", host, port, database));
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+
         return dataSource;
     }
 }
