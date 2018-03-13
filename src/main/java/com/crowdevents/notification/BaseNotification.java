5717844d98477b66305fa49cc3468b84aced3b6f
Seregy <seregy77@gmail.com>
2018-03-14 00:29:41 +0200

    Change table name for notifications to 'notifications'

--- src/main/java/com/crowdevents/notification/BaseNotification.java
+++ src/main/java/com/crowdevents/notification/BaseNotification.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
-@Entity
+@Entity(name = "notification")
 public class BaseNotification {
     @Id
     @Column(unique = true)
