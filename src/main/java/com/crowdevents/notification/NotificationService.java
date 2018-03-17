21a9cb2d83bc3dfebceaf3b4e5d39bce2dee852c
Seregy <seregy77@gmail.com>
2018-03-17 23:18:39 +0200

    Fix methods for sending notifications in NotificationService
    - Fix incorrect return types
    - Move LocalDateTime generation into these methods

--- src/main/java/com/crowdevents/notification/NotificationService.java
+++ src/main/java/com/crowdevents/notification/NotificationService.java
@@ -4,14 +4,11 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface NotificationService {
-    BaseNotification sendBaseNotification(String message, UUID receiverId,
-                                         LocalDateTime dateTime, UUID projectId);
+    BaseNotification sendBaseNotification(String message, UUID receiverId, UUID projectId);
     ContributionNotification sendContributionNotification(String message, UUID contributionId, UUID receiverId,
-                                         LocalDateTime dateTime, UUID projectId);
-    ContributionNotification sendPersonNotification(String message, UUID personId, UUID receiverId,
-                                                         LocalDateTime dateTime, UUID projectId);
-    ContributionNotification sendUpdateNotification(String message, UUID updateId, UUID receiverId,
-                                                    LocalDateTime dateTime, UUID projectId);
+                                                          UUID projectId);
+    PersonNotification sendPersonNotification(String message, UUID personId, UUID receiverId, UUID projectId);
+    UpdateNotification sendUpdateNotification(String message, UUID updateId, UUID receiverId, UUID projectId);
     BaseNotification get(UUID id);
     void delete(UUID id);
     void changeMessage(UUID id, String newMessage);
