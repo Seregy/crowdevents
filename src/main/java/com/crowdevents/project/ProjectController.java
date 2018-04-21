db07e5a5fe94a91bc494ed1359b0d868f7ab9c8d
Seregy77 <seregy77@gmail.com>
2018-04-22 02:32:51 +0300

    Update endpoints for project
    - Set api version to v0
    - Use custom params for listing projects instead of pageable
    - Add endpoint for updating existing project

--- src/main/java/com/crowdevents/project/ProjectController.java
+++ src/main/java/com/crowdevents/project/ProjectController.java
@@ -3,18 +3,20 @@ package com.crowdevents.project;
 import com.crowdevents.core.web.PageResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.util.UUID;
+import java.util.Map;
+import java.util.Optional;
+
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("v0/projects")
 @CrossOrigin
 public class ProjectController {
     private ProjectService projectService;
@@ -26,24 +28,56 @@ public class ProjectController {
         this.modelMapper = modelMapper;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     @ResponseBody
-    public PageResource<ProjectResource> getAllProjects(@PageableDefault(value = 30) Pageable pageable,
-                                                        @RequestAttribute(name = "starting_after",
-                                                                required = false) Long startingAfterId) {
-        return new PageResource<>(
-                projectService.getAll(pageable)
-                        .map((project) -> modelMapper.map(project, ProjectResource.class)));
+    public PageResource<ProjectResource> getAllProjects(@RequestParam(name = "page", defaultValue = "0")
+                                                                int pageNumber,
+                                                        @RequestParam(name = "limit", defaultValue = "10")
+                                                                int limit,
+                                                        @RequestParam(name = "starting_after", required = false)
+                                                                Long startingAfter,
+                                                        @RequestParam(name = "ending_before", required = false)
+                                                                Long endingBefore) {
+        Page<Project> resultPage;
+        PageRequest pageRequest = PageRequest.of(pageNumber, limit);
+        if (startingAfter == null && endingBefore == null) {
+            resultPage = projectService.getAll(pageRequest);
+        } else {
+            resultPage = projectService.getAllBeforeAndOrAfter(endingBefore, startingAfter, pageRequest);
+        }
+
+        return new PageResource<>(resultPage.map((project) -> modelMapper.map(project, ProjectResource.class)));
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity addProject(@RequestBody ProjectResource project, HttpServletRequest servletRequest) {
-        Project newProject = projectService.create(project.getName(), project.getDescription(),
-                project.getFundingGoal(), project.getOwners().toArray(new Long[] {}));
+    @GetMapping(value = "/{id}")
+    @ResponseBody
+    public ResponseEntity<ProjectResource> getProject(@PathVariable("id") Long id) {
+        return projectService.get(id)
+                .map(project -> ResponseEntity.ok(modelMapper.map(project, ProjectResource.class)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity createProject(@RequestBody ProjectResource newProject, HttpServletRequest servletRequest) {
+        Project createdProject = projectService.create(newProject.getName(), newProject.getDescription(),
+                newProject.getFundingGoal(), newProject.getOwners().toArray(new Long[] {}));
         URI uri = ServletUriComponentsBuilder.fromServletMapping(servletRequest)
-                .path("/projects/{id}")
-                .buildAndExpand(newProject.getId())
+                .path("/v0/projects/{id}")
+                .buildAndExpand(createdProject.getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity updateProject(@PathVariable("id") Long id, @RequestBody Map<String, Object> patchValues) {
+        Optional<Project> project = projectService.get(id);
+        if (project.isPresent()) {
+            ProjectResource projectResource = modelMapper.map(project, ProjectResource.class);
+            modelMapper.map(patchValues, projectResource);
+            projectService.update(id, modelMapper.map(projectResource, Project.class));
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
