4fbc31015b5af39ba1d89cc0aad6993099a1b969
Seregy77 <seregy77@gmail.com>
2018-04-22 12:35:20 +0300

    Add configuration for mapping LinkedHashMap to ProjectResource

--- src/main/java/com/crowdevents/core/MappingConfiguration.java
+++ src/main/java/com/crowdevents/core/MappingConfiguration.java
@@ -4,18 +4,25 @@ import com.crowdevents.category.Category;
 import com.crowdevents.comment.Comment;
 import com.crowdevents.contribution.Contribution;
 import com.crowdevents.faq.Faq;
+import com.crowdevents.location.Location;
 import com.crowdevents.person.Person;
+import com.crowdevents.project.ProjectResource;
 import com.crowdevents.reward.Reward;
 import com.crowdevents.update.Update;
+import org.joda.money.Money;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 
 @Configuration
 public class MappingConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
+        addProjectMaps(mapper);
         addPersonLongTypeMap(mapper);
         addContributionLongTypeMap(mapper);
         addCommentLongTypeMap(mapper);
@@ -24,6 +31,38 @@ public class MappingConfiguration {
         addUpdateLongTypeMap(mapper);
         addRewardLongTypeMap(mapper);
         return mapper;
+    }
+
+    public void addProjectMaps(ModelMapper mapper) {
+        mapper.createTypeMap(LinkedHashMap.class, ProjectResource.class)
+                .setProvider(request -> new ProjectResource())
+                .setConverter(context -> {
+                    LinkedHashMap source = context.getSource();
+                    ProjectResource result = context.getDestination();
+                    if (source.containsKey("name")) {
+                        result.setName((String) source.get("name"));
+                    } else if (source.containsKey("description")) {
+                        result.setDescription((String) source.get("description"));
+                    } else if (source.containsKey("location")) {
+                        Object value = source.get("location");
+                        Location location = value == null ? null : mapper.map(value, Location.class);
+                        result.setLocation(location);
+                    } else if (source.containsKey("start_date_time")) {
+                        Object value = source.get("start_date_time");
+                        LocalDateTime dateTime = value == null ? null : mapper.map(value, LocalDateTime.class);
+                        result.setStartDateTime(dateTime);
+                    } else if (source.containsKey("end_date_time")) {
+                        Object value = source.get("end_date_time");
+                        LocalDateTime dateTime = value == null ? null : mapper.map(value, LocalDateTime.class);
+                        result.setEndDateTime(dateTime);
+                    } else if (source.containsKey("funding_goal")) {
+                        Object value = source.get("funding_goal");
+                        Money money = value == null ? null : mapper.map(value, Money.class);
+                        result.setFundingGoal(money);
+                    }
+
+                    return result;
+                });
     }
 
     public void addPersonLongTypeMap(ModelMapper mapper) {
