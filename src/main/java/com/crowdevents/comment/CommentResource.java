37df36616c00a3387150f7e19300682a573b797e
Seregy77 <seregy77@gmail.com>
2018-04-29 15:10:43 +0300

    Add JsonView and JsonIgnoreProperties annotations to CommentResource

--- src/main/java/com/crowdevents/comment/CommentResource.java
+++ src/main/java/com/crowdevents/comment/CommentResource.java
@@ -3,17 +3,23 @@ package com.crowdevents.comment;
 import com.crowdevents.core.web.Views;
 import com.crowdevents.person.PersonResource;
 import com.crowdevents.project.ProjectResource;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import java.time.LocalDateTime;
 
+@JsonView(Views.Detailed.class)
 public class CommentResource {
     @JsonView(Views.Minimal.class)
     private Long id;
 
+    @JsonView(Views.Minimal.class)
+    @JsonIgnoreProperties({"gallery_videos", "gallery_images", "owners", "subscribers",
+            "contributions", "comments", "faqs", "categories", "updates", "rewards"})
     private ProjectResource project;
 
     @JsonView(Views.Minimal.class)
+    @JsonIgnoreProperties({"password", "email", "country", "city"})
     private PersonResource author;
 
     @JsonView(Views.Minimal.class)
