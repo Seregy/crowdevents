bc9fa50959b44547173e3288d3d0d1f2d14248a8
Seregy77 <seregy77@gmail.com>
2018-04-29 15:09:43 +0300

    Mark message column in Comment as LOB

--- src/main/java/com/crowdevents/comment/Comment.java
+++ src/main/java/com/crowdevents/comment/Comment.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -25,6 +26,7 @@ public class Comment {
     @ManyToOne
     private Person author;
 
+    @Lob
     @Column(nullable = false)
     private String message;
 
