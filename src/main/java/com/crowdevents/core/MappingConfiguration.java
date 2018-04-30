deeda2bce8498623be9e420931043da6c65613b2
Seregy77 <seregy77@gmail.com>
2018-04-30 13:54:59 +0300

    Configure mapping from LinkedHashMap to RewardResource

--- src/main/java/com/crowdevents/core/MappingConfiguration.java
+++ src/main/java/com/crowdevents/core/MappingConfiguration.java
@@ -1,10 +1,13 @@
 package com.crowdevents.core;
 
+import com.crowdevents.contribution.ContributionResource;
 import com.crowdevents.location.LocationResource;
 import com.crowdevents.project.ProjectResource;
+import com.crowdevents.reward.RewardResource;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.joda.money.Money;
 import org.modelmapper.ModelMapper;
@@ -22,6 +25,7 @@ public class MappingConfiguration {
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
         addProjectMaps(mapper);
+        addRewardMap(mapper);
         return mapper;
     }
 
@@ -60,6 +64,58 @@ public class MappingConfiguration {
                         Object value = source.get("funding_goal");
                         Money money = value == null ? null : mapper.map(value, Money.class);
                         result.setFundingGoal(money);
+                    }
+
+                    return result;
+                });
+    }
+
+    /**
+     * Adds mapping configurations for mapping reward and
+     * related to it classes.
+     *
+     * @param mapper model mapper instance
+     */
+    public void addRewardMap(ModelMapper mapper) {
+        mapper.createTypeMap(LinkedHashMap.class, RewardResource.class)
+                .setProvider(request -> new RewardResource())
+                .setConverter(context -> {
+                    LinkedHashMap source = context.getSource();
+                    RewardResource result = context.getDestination();
+                    if (source.containsKey("project")) {
+                        ProjectResource value = (ProjectResource) source.get("project");
+                        result.setProject(value);
+                    }
+
+                    if (source.containsKey("description")) {
+                        String value = (String) source.get("description");
+                        result.setDescription(value);
+                    }
+
+                    if (source.containsKey("contributions")) {
+                        @SuppressWarnings("unchecked")
+                        List<ContributionResource> value = (List) source.get("contributions");
+                        result.setContributions(value);
+                    }
+
+                    if (source.containsKey("minimal_contribution")) {
+                        Money value = (Money) source.get("minimal_contribution");
+                        result.setMinimalContribution(value);
+                    }
+
+                    if (source.containsKey("maximum_amount")) {
+                        Integer value = (Integer) source.get("maximum_amount");
+                        result.setMaximumAmount(value);
+                    }
+
+                    if (source.containsKey("delivery_date")) {
+                        String value = (String) source.get("delivery_date");
+                        result.setDeliveryDate(value);
+                    }
+
+                    if (source.containsKey("shipped_to")) {
+                        String value = (String) source.get("shipped_to");
+                        result.setShippedTo(value);
                     }
 
                     return result;
