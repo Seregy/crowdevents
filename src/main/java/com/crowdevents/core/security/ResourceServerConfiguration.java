dca4266ae814ffc8e5ef68b95f96a578769eef36
Seregy <seregy77@gmail.com>
2018-06-15 23:27:58 +0300

    Configure CORS for resource server

--- src/main/java/com/crowdevents/core/security/ResourceServerConfiguration.java
+++ src/main/java/com/crowdevents/core/security/ResourceServerConfiguration.java
@@ -1,11 +1,18 @@
 package com.crowdevents.core.security;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -29,5 +36,35 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.tokenStore(tokenStore);
+    }
+
+    /**
+     * Registers CorsFilter for resource server's API endpoints.
+     *
+     * @return FilterRegistrationBean for resource server
+     */
+    @Bean
+    public FilterRegistrationBean resourceCorsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(resourceCorsConfigurationSource()));
+        bean.setOrder(SecurityProperties.DEFAULT_FILTER_ORDER);
+        return bean;
+    }
+
+    /**
+     * Configures Cors for resource server's API endpoints.
+     *
+     * @return CorsConfigurationSource for resource server
+     */
+    @Bean
+    public CorsConfigurationSource resourceCorsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/v0/**", config);
+        return source;
     }
 }
